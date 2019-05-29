3d7f5a6b73c998fa6e718463576e3d2e85433965
Prodigy00 <simplygiddy@gmail.com>
2019-05-29 12:26:05 +0100

    added Delete button and handled delete event

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,34 +1,46 @@
-import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
-	state = {
-		movies: getMovies()
-	};
-	render() {
-		return (
-			<table className="table">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Genre</th>
-						<th>Stock</th>
-						<th>Rate</th>
-					</tr>
-				</thead>
-				<tbody>
-					{this.state.movies.map((movie) => (
-						<tr>
-							<td>{movie.title}</td>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>{movie.dailyRentalRate}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		);
-	}
+  state = {
+    movies: getMovies()
+  };
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map(movie => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default Movies;
